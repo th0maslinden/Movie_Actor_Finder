78b22cc323b18b7867d9f2c1068de9a68b9f8bed
lind0013 <thomas.linden1@etudiant.univ-reims.fr>
2023-06-12 15:53:10 +0200

    ajout movie.php et fonctions de bases

---
+++ src/Entity/movie.php
@@ -0,0 +1,163 @@
+<?php
+
+namespace Super\Sae201\Entity;
+
+use Cassandra\Date;
+
+class movie
+{
+    private int $id;
+    private int $posterId;
+    private string $originalLanguage;
+    private string $originalTitle;
+    private string $overview;
+    private date $releaseDate;
+    private int $runtime;
+    private string $tagline;
+    private string $title;
+
+
+    /**
+     * @return int
+     */
+    public function getId(): int
+    {
+        return $this->id;
+    }
+
+    /**
+     * @param int $id
+     */
+    public function setId(int $id): void
+    {
+        $this->id = $id;
+    }
+
+    /**
+     * @return int
+     */
+    public function getPosterId(): int
+    {
+        return $this->posterId;
+    }
+
+    /**
+     * @param int $posterId
+     */
+    public function setPosterId(int $posterId): void
+    {
+        $this->posterId = $posterId;
+    }
+
+    /**
+     * @return string
+     */
+    public function getOriginalLanguage(): string
+    {
+        return $this->originalLanguage;
+    }
+
+    /**
+     * @param string $originalLanguage
+     */
+    public function setOriginalLanguage(string $originalLanguage): void
+    {
+        $this->originalLanguage = $originalLanguage;
+    }
+
+    /**
+     * @return string
+     */
+    public function getOriginalTitle(): string
+    {
+        return $this->originalTitle;
+    }
+
+    /**
+     * @param string $originalTitle
+     */
+    public function setOriginalTitle(string $originalTitle): void
+    {
+        $this->originalTitle = $originalTitle;
+    }
+
+    /**
+     * @return string
+     */
+    public function getOverview(): string
+    {
+        return $this->overview;
+    }
+
+    /**
+     * @param string $overview
+     */
+    public function setOverview(string $overview): void
+    {
+        $this->overview = $overview;
+    }
+
+    /**
+     * @return Date
+     */
+    public function getReleaseDate(): Date
+    {
+        return $this->releaseDate;
+    }
+
+    /**
+     * @param Date $releaseDate
+     */
+    public function setReleaseDate(Date $releaseDate): void
+    {
+        $this->releaseDate = $releaseDate;
+    }
+
+    /**
+     * @return int
+     */
+    public function getRuntime(): int
+    {
+        return $this->runtime;
+    }
+
+    /**
+     * @param int $runtime
+     */
+    public function setRuntime(int $runtime): void
+    {
+        $this->runtime = $runtime;
+    }
+
+    /**
+     * @return string
+     */
+    public function getTagline(): string
+    {
+        return $this->tagline;
+    }
+
+    /**
+     * @param string $tagline
+     */
+    public function setTagline(string $tagline): void
+    {
+        $this->tagline = $tagline;
+    }
+
+    /**
+     * @return string
+     */
+    public function getTitle(): string
+    {
+        return $this->title;
+    }
+
+    /**
+     * @param string $title
+     */
+    public function setTitle(string $title): void
+    {
+        $this->title = $title;
+    }
+}
