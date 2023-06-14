a03fd403ea1e4f4d54899da12d2624ff8b5daee0
lind0013 <thomas.linden1@etudiant.univ-reims.fr>
2023-06-14 09:51:53 +0200

    ajout des escapesString

--- public/index.php
+++ public/index.php
@@ -14,7 +14,7 @@ $Movies = MovieCollection::findAll();
 $Images = ImageCollection::findAll();
 
 foreach ($Movies as $index => $movie) {
-    $html->appendContent("<a href=\"DescriptionMovie.php?MovieId={$movie->getId()}\"><li><div class='txt'>{$movie->getTitle()}</div><img src='data:image/jpeg;base64," . base64_encode(Image::findById($movie->getPosterId())->getJpeg()) . "' alt='Image'></li>");
+    $html->appendContent("<a href=\"DescriptionMovie.php?MovieId={$movie->getId()}\"><li><div class='txt'>{$html->escapeString("{$movie->getTitle()}")}</div><img src='data:image/jpeg;base64," . base64_encode(Image::findById($movie->getPosterId())->getJpeg()) . "' alt='Image'></li>");
 }
 $html->appendContent("</a></ul></div><footer>Dernière modification : 13/06/2023 - 15:18</footer>");
 echo $html->toHTML();
